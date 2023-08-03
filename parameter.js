4e3795387b54ba91992db59d15be4641f955c355
Imran <webimran2021@gmail.com>
2023-08-03 16:58:04 +0600

    more added fuction

--- parameter.js
+++ parameter.js
@@ -5,3 +5,27 @@ function bringSingara(money) {
 
 // call the function
 bringSingara(100);
+
+
+// agin try this parameter
+function bringSingara(money) {
+    console.log('ato taka disen:', money);
+    console.log('ai nan singara');
+}
+
+// call the function
+bringSingara(100);
+function add(num1, num2){
+    console.log('going to add:', num1, num2);
+}
+
+    // add(125, 96);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+}
+console.log(sum);
+
+// call the function
+sum(15, 98, 56, 44, 55)
---
+++ return.js
@@ -0,0 +1,26 @@
+function add(number1, number2) {
+    console.log(number1, number2);
+    var sum = number1 + number2;
+    console.log(sum);
+    return sum;
+    return 15;
+    return 'hello done';
+    return 'I am hungry';
+}
+
+add(45, 15 );
+
+var total = add(80, 20);
+console.log('total', total);
+
+
+// others way
+function bringSingara(money) {
+    var singaraPrice = 10;
+    var quality = money / singaraPrice;
+    return quantity;
+}
+
+var myTaka = 150;
+var singaras = bringSingara(myTaka);
+console.log('Eating singaras', singaras);
